2023dc6d4778bd8fcdce8027a8e7636586ad9fc7
ishwork <ishworkhadka70@gmail.com>
2026-02-13 10:22:50 +0200

    type: add types post

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export type BlogPost = {
+  _id: string;
+  title: string;
+  slug: {
+    current: string;
+  };
+  author: string;
+  mainImage: {
+    asset: {
+      _ref: string;
+    };
+  };
+  publishedAt: string;
+  body: Array<
+    | {
+        _type: 'block';
+        children: Array<{
+          text: string;
+        }>;
+      }
+    | {
+        _type: 'image';
+        asset: {
+          _ref: string;
+        };
+        alt?: string;
+      }
+  >;
+};
