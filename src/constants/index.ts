4c75c016bb2d447c4a3dd6faea9a1a77f5da0778
ishwork <ishworkhadka70@gmail.com>
2026-02-13 10:19:43 +0200

    feat: add icons and images

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+const FAVICON_ICON = '/favicon/blogHub.ico';
+const BLOG_NAME = '/images/blogHub.svg';
+const LOGO_IMAGE = '/images/logo.svg';
+const SUN_ICON = '/images/sun-icon.svg';
+const MOON_ICON = '/images/moon-icon.svg';
+
+export { FAVICON_ICON, BLOG_NAME, LOGO_IMAGE, SUN_ICON, MOON_ICON };
