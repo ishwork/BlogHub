cdd31a5c6db1dd93e65b8d713e449abe1a9456eb
ishwork <ishworkhadka70@gmail.com>
2026-04-24 11:51:45 +0300

    enhance: align TanStack Query staleTime with Cache-Control max-age

--- src/components/QueryProvider.tsx
+++ src/components/QueryProvider.tsx
@@ -8,7 +8,10 @@ type QueryProviderProps = {
 };
 
 const QueryProvider = ({ children }: QueryProviderProps) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    // avoid refetching immediately on the client-side ( default stale time is 0 seconds)
+    () => new QueryClient({ defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } } }),
+  );
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
